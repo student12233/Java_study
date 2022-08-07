829bcbb0367a23d3bb000dab7c441c4e397c9b6e
student12233 <zzzdanilxxx@bk.ru>
2022-08-07 12:19:24 +0300

    задание 1

--- mpj1/src/mpj1/program.java
+++ mpj1/src/mpj1/program.java
@@ -4,9 +4,20 @@ public class program {
 	public static void main(String args[])
 	{
 		MPI.Init(args);
-		int rank= MPI.COMM_WORLD.Rank();
+		int myrank= MPI.COMM_WORLD.Rank();
 		int size= MPI.COMM_WORLD.Size();
-		System.out.println(rank);
+		int message=myrank;
+		if((myrank % 2) == 0)
+		{
+			if((myrank + 1) != size)
+				MPI.COMM_SELF.Send_init(args, 2,0 ,MPI.INT, myrank+1, message);
+		
+			}else
+			{ if(myrank != 0)
+				MPI.COMM_SELF.Recv_init(args, 2,0 ,MPI.INT, myrank-1, message);
+				System.out.println(message);
+		}
+
 		MPI.Finalize();
 	}
 
