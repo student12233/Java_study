ef77e970cddc0af8423fabd53589480150e1918f
student12233 <zzzdanilxxx@bk.ru>
2022-08-25 19:12:32 +0300

    задание 2

--- mpj1/src/mpj1/program.java
+++ mpj1/src/mpj1/program.java
@@ -3,19 +3,31 @@ import mpi.*;
 public class program {
 	public static void main(String args[])
 	{
-		MPI.Init(args);
-		int myrank= MPI.COMM_WORLD.Rank();
-		int size= MPI.COMM_WORLD.Size();
-		int message=myrank;
-		if((myrank % 2) == 0)
-		{
-			if((myrank + 1) != size)
-				MPI.COMM_SELF.Send_init(args, 2,0 ,MPI.INT, myrank+1, message);
 		
-			}else
-			{ if(myrank != 0)
-				MPI.COMM_SELF.Recv_init(args, 2,0 ,MPI.INT, myrank-1, message);
-				System.out.println(message);
+		MPI.Init(args);
+		int S=0;
+		int myrank= MPI.COMM_WORLD.Rank();
+		int buff =myrank;
+		int[] message_in= {buff};
+		int[] message_out= {1};
+		if (myrank==0)
+		{
+			message_in[0]=buff;
+			MPI.COMM_WORLD.Send(message_in, 0, 1, MPI.INT, myrank+1, 1);
+			MPI.COMM_WORLD.Recv(message_out, 0, 1, MPI.INT, MPI.COMM_WORLD.Size()-1, 1);
+			S=message_out[0];
+			System.out.println(S);
+			
+		}
+		else 
+		{
+			MPI.COMM_WORLD.Recv(message_out, 0, 1, MPI.INT, myrank-1, 1);
+			S=message_out[0]+myrank;
+			message_in[0]= S;
+			if(myrank<MPI.COMM_WORLD.Size()-1)
+			MPI.COMM_WORLD.Send(message_in, 0, 1, MPI.INT, myrank+1, 1);
+			else
+			MPI.COMM_WORLD.Send(message_in, 0, 1, MPI.INT,0, 1);	
 		}
 
 		MPI.Finalize();
