6a371047502b86afdae9aecb1f9718a9fdda7a2b
student12233 <zzzdanilxxx@bk.ru>
2022-09-02 12:59:55 +0300

    Задание 2 часть 2

--- mpj1/src/mpj1/program.java
+++ mpj1/src/mpj1/program.java
@@ -8,26 +8,30 @@ public class program {
 		int S=0;
 		int myrank= MPI.COMM_WORLD.Rank();
 		int buff =myrank;
-		int[] message_in= {buff};
-		int[] message_out= {1};
+		int[] message= new int[1];
 		if (myrank==0)
 		{
-			message_in[0]=buff;
-			MPI.COMM_WORLD.Send(message_in, 0, 1, MPI.INT, myrank+1, 1);
-			MPI.COMM_WORLD.Recv(message_out, 0, 1, MPI.INT, MPI.COMM_WORLD.Size()-1, 1);
-			S=message_out[0];
+			message[0]=buff;
+			MPI.COMM_WORLD.Isend(message, 0, 1, MPI.INT, myrank+1, 1);
+			Request r=MPI.COMM_WORLD.Irecv(message, 0, 1, MPI.INT, MPI.COMM_WORLD.Size()-1, 1);
+			S= myrank;
+		    r.Wait();
+		    S=S+message[0];
 			System.out.println(S);
 			
 		}
 		else 
 		{
-			MPI.COMM_WORLD.Recv(message_out, 0, 1, MPI.INT, myrank-1, 1);
-			S=message_out[0]+myrank;
-			message_in[0]= S;
+		    Request r=	MPI.COMM_WORLD.Irecv(message, 0, 1, MPI.INT, myrank-1, 1);
+			
+			S=myrank;
+			r.Wait();
+			S = S+message[0];
+			message[0]=S;
 			if(myrank<MPI.COMM_WORLD.Size()-1)
-			MPI.COMM_WORLD.Send(message_in, 0, 1, MPI.INT, myrank+1, 1);
+			MPI.COMM_WORLD.Isend(message, 0, 1, MPI.INT, myrank+1, 1);
 			else
-			MPI.COMM_WORLD.Send(message_in, 0, 1, MPI.INT,0, 1);	
+			MPI.COMM_WORLD.Isend(message, 0, 1, MPI.INT,0, 1);	
 		}
 
 		MPI.Finalize();
