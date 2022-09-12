f8d0b206db11849cbabd57cd576c62bd4f44698c
student12233 <zzzdanilxxx@bk.ru>
2022-09-12 17:22:05 +0300

    задание 4

--- mpj1/src/mpj1/program.java
+++ mpj1/src/mpj1/program.java
@@ -4,37 +4,38 @@ public class program {
 	public static void main(String args[])
 	{
 		
+		int data[] = new int[1];
+		int buf[] = {1,3,5};
+		int count,TAG = 0;
+		Status st;
+		data[0] = 2016;
 		MPI.Init(args);
-		int S=0;
-		int myrank= MPI.COMM_WORLD.Rank();
-		int buff =myrank;
-		int[] message= new int[1];
-		if (myrank==0)
+		int rank = MPI.COMM_WORLD.Rank();
+		int size=MPI.COMM_WORLD.Size();
+		if(rank == 0)
 		{
-			message[0]=buff;
-			MPI.COMM_WORLD.Isend(message, 0, 1, MPI.INT, myrank+1, 1);
-			Request r=MPI.COMM_WORLD.Irecv(message, 0, 1, MPI.INT, MPI.COMM_WORLD.Size()-1, 1);
-			S= myrank;
-		    r.Wait();
-		    S=S+message[0];
-			System.out.println(S);
-			
+		MPI.COMM_WORLD.Send(data, 0, 1, MPI.INT, 2, TAG);
 		}
-		else 
-		{
-		    Request r=	MPI.COMM_WORLD.Irecv(message, 0, 1, MPI.INT, myrank-1, 1);
-			
-			S=myrank;
-			r.Wait();
-			S = S+message[0];
-			message[0]=S;
-			if(myrank<MPI.COMM_WORLD.Size()-1)
-			MPI.COMM_WORLD.Isend(message, 0, 1, MPI.INT, myrank+1, 1);
-			else
-			MPI.COMM_WORLD.Isend(message, 0, 1, MPI.INT,0, 1);	
+		else if(rank == 1){
+		MPI.COMM_WORLD.Send(buf, 0, buf.length, MPI.INT, 2, TAG);
 		}
-
-		MPI.Finalize();
+		else if(rank == 2){
+		st = MPI.COMM_WORLD.Probe(0,TAG);
+		count = st.Get_count(MPI.INT);
+		int[] back_buf=new int[count];
+		MPI.COMM_WORLD.Recv(back_buf,0,count,MPI.INT,0,TAG);
+		System.out.println("Rank = 0 ");
+		for(int i = 0 ; i < count ; i ++)
+		System.out.print(back_buf[i]+" ");
+		st = MPI.COMM_WORLD.Probe(1,TAG);
+		count = st.Get_count(MPI.INT);
+		int[] back_buf2=new int[count];
+		MPI.COMM_WORLD.Recv(back_buf2,0,count,MPI.INT,1,TAG);
+		System.out.println("Rank = 1 ");
+		for(int i = 0 ; i < count ; i ++)
+		System.out.print(back_buf2[i]+" ");
+		}
+		MPI.Finalize(); 
 	}
 
 }
